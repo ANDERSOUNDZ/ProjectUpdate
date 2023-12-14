f5e0ef65990151ab9037f49a7bcec43baf29498e
Anderson Chanchay <andersonmikol@live.com>
2023-12-14 18:15:50 -0500

    Nuevo project, delete old project

--- CtApp/src/app/app.component.spec.ts
+++ CtApp/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'bases' title`, () => {
+  it(`should have the 'CtApp' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('bases');
+    expect(app.title).toEqual('CtApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, bases');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, CtApp');
   });
 });
